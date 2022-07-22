e3ea943bb90d17139e1ace6a66e2587178cb8d46
Chadrac-WAKOMYA <chadracx@gmail.com>
2022-07-22 11:06:32 +0200

    basic endpoints finished

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/',(req,res)=>res.send("Hello, Express ! "))
 
+app.get('/api/pokemons/:id',(req, res) => {
+    const id =  parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.json(pokemon)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    let totalPokemon = pokemons.length
+    res.send(`Il y a ${totalPokemon} pokemons dans le pokendex pour le moment`)
+})
 
 // app.get('/api/pokemons/:id',(req,res)=>{
 //     const id = req.params.id
