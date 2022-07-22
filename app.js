aa96fb3b99537a0f38c1964519eb3fcf7cb2dd7a
Chadrac-WAKOMYA <chadracx@gmail.com>
2022-07-22 08:41:30 +0200

    return at the bigining

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express')
-let pokemons = require('./mock-pokemon');
 
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>res.send("Hello again, Express ! "))
+app.get('/',(req,res)=>res.send("Hello, Express ! "))
 
 
-app.get('/api/pokemons/:id',(req,res)=>{
-    const id = req.params.id
-    const pokemon = pokempns.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez demande le pokemon ${pokemon.name}`)
-})
-
+// app.get('/api/pokemons/:id',(req,res)=>{
+//     const id = req.params.id
+//     const pokemon = pokemons.find(pokemon => pokemon.id === id)
+//     res.send(`Vous avez demande le pokemon ${pokemon.name}`)
+// })
 
 app.listen(port,()=>console.log(`Notre application Node est demaree sur : http://localhost:${port}`))
