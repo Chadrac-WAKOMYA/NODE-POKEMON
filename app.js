25b6749d59355d534a5b517d062edcc4cde463ff
Chadrac-WAKOMYA <chadracx@gmail.com>
2022-08-05 12:56:02 +0200

    crud finished

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ sequelize.initDb()
 require('./src/routes/findAllPokemons')(app)
 require('./src/routes/findPokemonByPK')(app)
 require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
 
 
 
